60ec32a5c4de91632b8fd23853ff2395c622fccf
Håkon Østerbø <hauk88@gmail.com>
2024-10-01 17:08:33 +0200

    fix vite-build stuff

--- webappvite/src/CardView.tsx
+++ webappvite/src/CardView.tsx
@@ -8,7 +8,8 @@ type CardProps = {
 const CardView = ({ card, preferAlt }: CardProps) => {
   const img = preferAlt ? card.alt_image ?? card.image : card.image;
 
-  const url = "/img/" + img;
+  const url = import.meta.env.BASE_URL + "img/" + img;
+
   return <img src={url} alt="Cant find" width="229px" height="357px" />;
 };
 
